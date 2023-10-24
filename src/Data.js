5d105f818d19a15de6888197c9a69dd6bcfc8dc2
Om Sharma <om@cyberclouds.com>
2023-10-24 16:00:09 +0530

    FirstUpdate

---
+++ src/Data.js
@@ -0,0 +1,46 @@
+const data = [
+    {
+        id:1,
+        name:"Agra",
+        info:"Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image:"https://cdn.thecodehelp.in/Agra.jpeg",
+        price:"35,756"
+    },
+    {
+        id:2,
+        name:"Jaipur",
+        info:"Jaipur is the capital of India’s Rajasthan state. It evokes the royal family that once ruled the region and that, in 1727, founded what is now called the Old City, or “Pink City” for its trademark building color.",
+        image:"https://cdn.thecodehelp.in/Jaipur.jpeg",
+        price:"85,756"
+    },
+    {
+        id:3,
+        name:"Goa",
+        info:"Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961 is evident in its preserved 17th-century churches and the area’s tropical spice plantations. Goa is also known for its beaches, ranging from popular stretches at Baga and Palolem to those in laid-back fishing villages such as Agonda. ",
+        image:"https://cdn.thecodehelp.in/Goa.jpeg",
+        price:"78,756"
+    },
+    {
+        id:4,
+        name:"Varanasi",
+        info:"Varanasi is a city in the northern Indian state of Uttar Pradesh dating to the 11th century B.C. Regarded as the spiritual capital of India, the city draws Hindu pilgrims who bathe in the Ganges River’s sacred waters and perform funeral rites. Along the city's winding streets are some 2,000 temples, including Kashi Vishwanath, the “Golden Temple,” dedicated to the Hindu god Shiva.",
+        image:"https://cdn.thecodehelp.in/Varanasi.jpeg",
+        price:"45,756"
+    },
+    {
+        id:5,
+        name:"Darjeeling",
+        info:"Darjeeling is a town in India's West Bengal state, in the Himalayan foothills. Once a summer resort for the British Raj elite, it remains the terminus of the narrow-gauge Darjeeling Himalayan Railway, or “Toy Train,” completed in 1881. It's famed for the distinctive black tea grown on plantations that dot its surrounding slopes. Its backdrop is Mt. Kanchenjunga, among the world’s highest peaks.",
+        image:"https://cdn.thecodehelp.in/Darjeeling.jpeg",
+        price:"67,756"
+    },
+    {
+        id:6,
+        name:"Jaisalmer",
+        info:"Jaisalmer is a former medieval trading center and a princely state in the western Indian state of Rajasthan, in the heart of the Thar Desert. Known as the 'Golden City,' it's distinguished by its yellow sandstone architecture. Dominating the skyline is Jaisalmer Fort, a sprawling hilltop citadel buttressed by 99 bastions. Behind its massive walls stand the ornate Maharaja's Palace and intricately carved Jain temples.",
+        image:"https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+        price:"89,756"
+    },
+];
+
+export default data;
